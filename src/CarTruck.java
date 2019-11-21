7fb8d3451cd444dd0e9fb31c6d543b4c49698944
Matilda <falkenby@student.chalmers.se>
2019-11-21 21:19:31 +0100

    CarTruck storage
    Having problems with setting the positions of the cars on the CarTruck.
    Also, the name workshop maybe was not the best name, but w/e, it is what it is.
    Trying to make Workshop also work with the storage.

--- src/CarTruck.java
+++ src/CarTruck.java
@@ -53,4 +53,5 @@ public class CarTruck extends Truck{
 
 
 
+
 }
--- src/Workshop.java
+++ src/Workshop.java
@@ -1,2 +1,58 @@
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
 public class Workshop {
+
+    private double storageMax;
+    private ArrayList<Car> cars;
+    private Point2D.Double carsPosition;
+
+    /**
+     * Creates a new arraylist for the cars
+     */
+    public Workshop(int carMax) {
+        storageMax = carMax;
+        this.cars = new ArrayList<Car>(carMax);
+    }
+
+    /**
+     * Returns the current position of the cars,
+     * for the CarTruck class to use
+     *
+     * @return
+     */
+    public Point2D.Double getCarsPosition() {
+        return carsPosition;
+    }
+
+    public void addCar(Car c){
+        if(cars.size() == (storageMax-1){
+            throw new RuntimeException("There isn't any room!!");
+        }
+        cars.add(c);
+    }
+
+    public void unloadTruck(){
+
+    }
+
+    public void checkOutCar(int index){
+        
+
+    }
+
+    public void setCarsPosition(Point2D.Double point) {
+        this.carsPosition = point;
+
+        for (Car c : getCars()) {
+            c.setCarsPosition(point);
+        }
+    }
+
+    public ArrayList<Car> getCars(){
+        return cars;
+    }
+
+
 }
